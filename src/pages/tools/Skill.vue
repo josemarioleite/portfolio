b9d2c1c08bb275f59ac3d10ee098cce646623a1d
josemarioleite <marioleitecomercial@gmail.com>
2024-04-12 01:31:08 -0400

    ...

--- src/pages/tools/Skill.vue
+++ src/pages/tools/Skill.vue
@@ -1,38 +1,44 @@
 <template>
 <div class="skill">
-  <q-img
+  <!-- <q-img
     class="skill__img"
     src="../../assets/langs/typescript.png"
     spinner-color="primary"
     spinner-size="82px"
   />
 
-  <span> Typescript </span>
+  <span> Typescript </span> -->
+  <SkillItem />
 </div>
 </template>
 
 <script lang="ts" setup>
+import SkillItem from './Skill_Item.vue'
 </script>
 
 <style lang="scss" scoped>
 .skill {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 150px;
-  width: 120px;
-  border-radius: 5px 5px 25px 25px;
-  border: 1px solid #ccc;
-
-  span {
-    margin-top: 5px;
-    color: #fff;
-  }
-
-  &__img {
-    width: 96px;
-    height: 96px;
-  }
+  height: max-content;
+  width: 100%;
 }
+// .skill {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   height: 150px;
+//   width: 120px;
+//   border-radius: 5px 5px 25px 25px;
+//   border: 1px solid #ccc;
+
+//   span {
+//     margin-top: 5px;
+//     color: #fff;
+//   }
+
+//   &__img {
+//     width: 96px;
+//     height: 96px;
+//   }
+// }
 </style>
---
+++ src/pages/tools/Skill_Item.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="item">
+    <q-carousel
+      v-model="style"
+      transition-prev="scale"
+      transition-next="scale"
+      swipeable
+      animated
+      control-color="white"
+      navigation
+      padding
+      arrows
+      height="550px"
+      class="bg-primary text-white shadow-1"
+    >
+      <template v-slot:navigation-icon="{ active, btnProps, onClick }">
+        <q-btn v-if="active" size="lg" icon="man" color="orange" flat round dense @click="onClick" />
+        <q-btn v-else size="sm" :icon="btnProps.icon" color="white" flat round dense @click="onClick" />
+      </template>
+
+      <q-carousel-slide name="style">
+        <q-img
+          class="skill__img"
+          src="../../assets/langs/typescript.png"
+          spinner-color="primary"
+          spinner-size="82px"
+        />
+        <!-- <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div> -->
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+let images = ref([])
+const style = ref('style')
+const lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
+
+const loadImages = () => {
+  const req: any = require
+  const context = req.context('../../assets/langs', false, /\.png$/)
+
+  images = context.keys().map(context)
+}
+
+onMounted(() => {
+  loadImages()
+})
+</script>
+
+<style lang="scss" scoped>
+.item {
+  width: 100%;
+}
+
+.skill__img {
+  z-index: 1;
+  height: 96px;
+  width: 96px;
+}
+</style>
